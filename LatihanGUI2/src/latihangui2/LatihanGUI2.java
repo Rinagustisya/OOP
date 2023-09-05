8a21980bb0238a04ef7b2e86c78aa314921f0c67
Rina Agustisyaningsih <113598878+Rinagustisya@users.noreply.github.com>
2023-09-05 12:18:59 +0700

    Latihan GUI

---
+++ LatihanGUI2/src/latihangui2/LatihanGUI2.java
@@ -0,0 +1,11 @@
+package latihangui2;
+public class LatihanGUI2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
